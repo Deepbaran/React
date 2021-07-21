6b748a38b198f5a0b5f4275efe703c497137013c
Deepbaran <deepbaran97@gmail.com>
2021-07-22 01:25:22 +0530

    fourth commit

--- one-helloworld/src/Button.js
+++ one-helloworld/src/Button.js
@@ -10,6 +10,7 @@ import './style.css';
 // };
 
 const Button = ({ title = 'Nothing' }) => (
+  //Here we are destructuring the props.
   //If no props/properties value for title is not passed then 'Nothing' will be used as the value of the prop title.
   <div>
     <button className="button">{title}</button>
